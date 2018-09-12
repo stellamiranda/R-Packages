1c13982ef4e2409b53c3f25fef1959c338cd41ee
Stella Miranda <stellamariam@gmail.com>
2018-09-11 22:39:53 -0500

    place correct URL

--- spec/latest_packages_spec.rb
+++ spec/latest_packages_spec.rb
@@ -4,7 +4,7 @@ require 'spec_helper'
 
 require_relative '../lib/latest_packages'
 
-#Test are fetching packages from this Sample page https://r-package-list.herokuapp.com/PACKAGES
+#Test are fetching packages from this Sample spec/samples/r_packages_list
 
 class TestPackageClass
 end
@@ -18,14 +18,14 @@ describe LatestPackages do
 
   context 'When #download_package_info method is called' do
     it 'should fetch package info from URL' do
-      expect(@dummy.download_latest_packages).to_not be_empty
+      expect(@dummy.download_latest_packages('spec/samples/r_packages_list')).to_not be_empty
     end
   end
 
   context 'When #download_latest_packages method is called' do
     it 'should download packages from URL' do
       sample = File.read('./spec/samples/download_latest_packages')
-      output = @dummy.download_latest_packages()
+      output = @dummy.download_latest_packages('spec/samples/r_packages_list')
       expect(output).to eq sample
     end
   end
@@ -42,9 +42,8 @@ describe LatestPackages do
   context 'When #latest_packages method is called' do
     it 'should return latest packages' do
       data = File.read('./spec/samples/download_latest_packages')
-      sample = { "ABCanalysis" => { :name => "ABCanalysis", :version => "1.2.1" } }
       output = @dummy.latest_packages
-      expect(output).to eq sample
+      expect(output).to eq data
     end
   end
 end
